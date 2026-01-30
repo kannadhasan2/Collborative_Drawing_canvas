fc7eaf5a2115c66dfe8a31be88fb1e078c41a901
kannadhasan <kannadhasansivakumar2005@gmail.com>
2026-01-30 11:38:59 +0530

    feat(canvas): mousemove event for cordinate map

--- client/canvas.js
+++ client/canvas.js
@@ -11,5 +11,32 @@ class DrawingCanvas{
 
         this.ctx = this.canvas.getContext('2d')
         this.overlayCtx = this.overlay.getContext('2d')
+
+        this.setupEventListeners()
+    }
+
+    setupEventListeners(){
+        this.canvas.addEventListener('mousemove', this.updateMousePosition.bind(this));
+
+    }
+
+    getMousePosition(event){
+        const rect = this.canvas.getBoundingClientRect()
+        const scaleX = this.canvas.width / rect.width 
+        const scaleY = this.canvas.height / rect.height 
+        return {
+            x:(event.clientX - rect.left) * scaleX,
+            y:(event.clientY - rect.top)*scaleY
+        }
+    }
+
+    updateMousePosition(event){
+        const position = this.getMousePosition(event) 
+        const mouseX = document.getElementById("mouse-x") 
+        const mouseY = document.getElementById("mouse-y")
+        if (mouseX && mouseY){
+            mouseX.textContent = Math.round(position.x)
+            mouseY.textContent = Math.round(position.y)
+        }
     }
 }
