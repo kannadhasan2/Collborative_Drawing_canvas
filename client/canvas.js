ab7eb6e228b939a5ee6f8cd636f322ac02173425
kannadhasan <kannadhasansivakumar2005@gmail.com>
2026-01-30 12:13:17 +0530

    update(canvas):setup canvas

--- client/canvas.js
+++ client/canvas.js
@@ -11,12 +11,34 @@ class DrawingCanvas{
 
         this.ctx = this.canvas.getContext('2d')
         this.overlayCtx = this.overlay.getContext('2d')
+        
+        // Drawing state
+        this.isDrawing = false;
+        this.lastX = 0;
+        this.lastY = 0;
+        this.currentColor = '#ff1e00';
+        this.currentTool = 'brush';
+        this.brushSize = 5;
 
+        this.setupCanvas()
         this.setupEventListeners()
     }
 
+    setupCanvas(){
+        // Set Canvas size to match the container 
+        const container = this.canvas.parentElement 
+        this.canvas.width = container.clientWidth 
+        this.canvas.height = container.clientHeight
+
+        // setup Overlay canvas 
+        this.overlay.width = container.clientWidth 
+        this.overlay.height = container.clientHeight 
+
+    }
+
     setupEventListeners(){
-        this.canvas.addEventListener('mousemove', this.updateMousePosition.bind(this));
+        this.canvas.addEventListener("mousedown",(event) => this.startDrawing(event) )
+        this.canvas.addEventListener('mousemove', (event) => this.updateMousePosition(event));
 
     }
 
@@ -38,5 +60,13 @@ class DrawingCanvas{
             mouseX.textContent = Math.round(position.x)
             mouseY.textContent = Math.round(position.y)
         }
+    }
+
+    startDrawing(event){
+        const position = this.getMousePosition(event)
+        this.isDrawing = true 
+        this.lastX = position.x 
+        this.lastY = position.y 
+        
     }
 }
