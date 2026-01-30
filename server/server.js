d8eff9169bc952e986b592dbb2e635774596ff41
kannadhasan <kannadhasansivakumar2005@gmail.com>
2026-01-30 09:09:34 +0530

    feat: initialize server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// Serve static files from client directory
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Serve index.html for all routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
